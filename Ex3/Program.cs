2715553029d20ce0f84c4c86e54f40c8f5845437
JustCheka <cheka201@yandxe.ru>
2023-08-22 15:54:17 +0300

    Ex 3

---
+++ Ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Напишите цифру от 1 до 7");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 1 || num > 7) Console.WriteLine("Something went wrong");
+else {
+    switch (num) {
+        case 6:
+            Console.WriteLine("Да");
+            break;
+        case 7:
+            Console.WriteLine("Да");
+            break;
+        default:
+            Console.WriteLine("Нет");
+            break;
+    }
+}
