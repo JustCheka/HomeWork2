82ec2955649ae237559996fd6f97a1bfc314d0ab
JustCheka <cheka201@yandxe.ru>
2023-08-22 15:36:45 +0300

    Ex 1 changes 1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -3,7 +3,7 @@ int num = int.Parse(Console.ReadLine());
 
 if (num < 100 || num > 999) Console.WriteLine("Число не трехзначное");
 else {
-    int result = num /= 10;
+    int result = num / 10;
     result %= 10;
     Console.WriteLine("Ответ " + result);
 }
