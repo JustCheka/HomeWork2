b6d94aded39034d9b06b68628291fbdcec743985
JustCheka <cheka201@yandxe.ru>
2023-08-22 15:33:51 +0300

    Ex 1

---
+++ Ex1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 100 || num > 999) Console.WriteLine("Число не трехзначное");
+else {
+    int result = num /= 10;
+    result %= 10;
+    Console.WriteLine("Ответ " + result);
+}
